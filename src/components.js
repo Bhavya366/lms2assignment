59421a2968b92a650f758d26ea68ca3c789d9b55
bhavya <bhavya.n2003@gmail.com>
2023-01-29 18:16:32 +0530

    first commit

---
+++ src/components.js
@@ -0,0 +1,30 @@
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker'
+
+function ComponentOne(props) {
+    return (
+            <div class="card">
+                <div class="content">
+                    <img class="right floated mini ui image" src={faker.image.avatar()} alt="alt" />
+                    <div class="header">
+                        {props.name}
+                    </div>
+                    <div class="meta">
+                        {props.friend}
+                    </div>
+                    <div class="description">
+                        {props.content}
+                    </div>
+                </div >
+            
+            <div class="extra content">
+                <div class="ui two buttons">
+                    <div class="ui basic green button">Approve</div>
+                    <div class="ui basic red button">Decline</div>
+                </div>
+            </div></div>
+    )
+}
+
+
+
+export default ComponentOne;
